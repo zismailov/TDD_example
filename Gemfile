21f88495794b6ad50fef18554e287da61ec21cfe
zulkarnay <bedelbaev@gmail.com>
2017-07-10 12:48:04 +0600

    add some test config

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
+gem 'bootstrap-sass'
 
 group :test do
   gem 'capybara'
--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,16 @@ module TDDExample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs:    false,
+        request_specs: false,
+        routing_specs: false,
+        helper_specs:  false
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil
